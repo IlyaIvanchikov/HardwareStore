156bff4e6b5926cc026614c6ee2a7e53caf5d751
IlyaIvanchikov <phantomonster1@mail.ru>
2019-12-02 17:15:58 +0200

    feat: add home page

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,6 @@
 import './style/main.css';
 import 'bootstrap';
+import './components/header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import myImage from './public/les.jpg';
 // $('body').css('background-color', 'red');
